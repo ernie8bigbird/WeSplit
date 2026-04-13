821fcf7da018cd3cd56a133f6e41e3f3f45c564f
ernie8bigbird <ernie8bigbird@hotmail.com>
2026-04-13 14:50:31 +1200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ernest Morgan on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
